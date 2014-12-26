1e54ab97b46db4bda4ffe5f0fcd97c7f2a636924
Nguyễn Hồng Phúc <scottfu89@gmail.com>
2014-12-26 12:26:48 +0700

    Update chuong 5
    full code chuong 5

--- Bai tap/Code/bai3_123.vhd
+++ Bai tap/Code/bai3_123.vhd
@@ -1,6 +1,7 @@
 -- Khai bao thu vien
 library ieee;
 use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.all;
 -- Khai bao thuc the
 entity bai3_123 is
     port (
@@ -13,10 +14,25 @@ architecture behavior of bai3_123 is
 subtype NewInt  is integer range -2 to 6;
 subtype NewFloat  is real range -5.3 to 16.2;
 type VOWEL  is ('u','e','o','a','i');
-subtype int is integer range 0 to 15;
+subtype int1 is integer range 0 to 15;
 type IntArray is array (1 downto 0,4 downto 0) of integer;
 type slv4_16 is array(3 downto 0) of std_logic_vector(15 downto 0) ;
 
 begin
-
+    process(clk)
+        variable NI : NewInt;
+        variable NF : NewFloat;
+        variable VO : VOWEL;
+        variable I1 : int1;
+        variable IA : IntArray;
+        variable SLV: slv4_16;
+    begin
+        NI := NI + 1;
+        NF := real(NI) + NF;
+        IA(1,4) := NI * I1;
+        VO := 'u';
+        SLV(0):= "1111000010101100";
+        SLV(2):= std_logic_vector(to_unsigned(I1,16));
+        I1 := to_integer(unsigned(SLV(3)));
+    end process;
 end behavior;
---
+++ Bai tap/Code/bai4_1.vhd
@@ -0,0 +1,28 @@
+-- Khai bao thu vien
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.NUMERIC_STD.all;
+-- Khai bao thuc the
+entity bai4_1 is
+    port (
+        input   : in  std_logic_vector(3 downto 0);
+        sel     : in std_logic_vector(1 downto 0);
+        output  : out std_logic
+    );
+end bai4_1;
+-- Khai bao cau truc
+architecture behavior of bai4_1 is
+-- Dinh nghia cac kieu du lieu
+
+begin
+    process(sel,input)
+    begin
+        case sel is
+            when '00' => output <= input(0);
+            when '01' => output <= input(1);
+            when '10' => output <= input(2);
+            when '11' => output <= input(3);
+            when others => output <= 0;
+        end case
+    end process;
+end behavior;
